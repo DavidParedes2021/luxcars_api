3077b1622388cb600740cf94c776761771d134fa
pauocar <paula.ocanas.car@gmail.com>
2024-11-19 20:17:26 -0600

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8002;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
